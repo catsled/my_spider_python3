2a7a3c447f5cea63566ae8c2e8d25174a00bee92
catsled <bigmaidong6@163.com>
2017-11-10 22:05:24 +0800

    qunar

--- splash_test_spider/splash_test_spider/items.py
+++ splash_test_spider/splash_test_spider/items.py
@@ -10,5 +10,14 @@ import scrapy
 
 class SplashTestSpiderItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    date = scrapy.Field()  # 日期
+    weekend = scrapy.Field()  # 周几
+    start_station = scrapy.Field()  # 出发站
+    end_station = scrapy.Field()  # 到达站
+    start_time = scrapy.Field()  # 发车时间
+    end_time = scrapy.Field()  # 到达时间
+    code = scrapy.Field()  # 车编号
+    spend_time = scrapy.Field()  # 所需时间
+    lowest_price = scrapy.Field()  # 最低票价
+    site_type = scrapy.Field()  # 座位类型
+    rest = scrapy.Field()  # 剩余票数
--- splash_test_spider/splash_test_spider/spiders/test.py
+++ splash_test_spider/splash_test_spider/spiders/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import base64
+import re
+from ..items import SplashTestSpiderItem
 from scrapy_splash import SplashRequest
 
 
@@ -10,25 +11,76 @@ class TestSpider(scrapy.Spider):
     allowed_domains = ['train.qunar.com']
     start_urls = ['http://touch.train.qunar.com/trainList.html?startStation=%E5%8C%97%E4%BA%AC&endStation=%E4%B8%8A%E6%B5%B7&date=2017-11-10&searchType=stasta&bd_source=qunar&filterTrainType=&filterTrainType=&filterTrainType=']
 
+    next_script = """
+            function main(splash)
+                splash:set_user_agent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1Query String Parametersview sourceview URL encoded')
+                assert(splash:go(splash.args.url))
+                splash:wait(3)
+                local element = splash:select("#btn-calendar-next")
+                local html_now = splash:html()
+                element:mouse_click()
+                splash:wait(5)
+                return {
+                    url = splash:url(),
+                    html = html_now,
+                }
+            end
+    """
+
+    tackle_method = {
+        1: 'self.get_time',
+        2: 'self.get_stations',
+        3: 'self.code_spend',
+        4: 'self.site_money_numbers',
+    }
+
+    # data_pattern = re.compile(r'(?<=data-href=")(.*?)"', re.S)
+
     def start_requests(self):
         for url in self.start_urls:
-            script = """
-            function main(splash)
- 	            splash:set_user_agent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1Query String Parametersview sourceview URL encoded')
-	            assert(splash:go('http://touch.train.qunar.com/trainList.html?startStation=%E5%8C%97%E4%BA%AC&endStation=%E4%B8%8A%E6%B5%B7&date=2017-11-10&searchType=stasta&bd_source=qunar&filterTrainType=&filterTrainType=&filterTrainType='))
-	            splash:wait(3)
-	            return splash:html()
-            end
-            """
-            splash_args = {
-                'lua_source': script,
-            }
-            yield SplashRequest(url, args={'lua_source': script}, endpoint='execute', callback=self.parse)
+            yield SplashRequest(url, args={'lua_source': self.next_script}, endpoint='execute', callback=self.parse)
 
     def parse(self, response):
-        print(response.body_as_unicode())
-        # print(response.request.headers)
+        ticket_info_list = response.xpath('//ul[@id="m-lists"]/li')
+        print('^'*20,response.url)
+        for ticket_info in ticket_info_list:
+            item = SplashTestSpiderItem()
+            ceil_info_list = ticket_info.xpath('./a/span')
+            for index, span in enumerate(ceil_info_list, 1):
+                eval(self.tackle_method.get(index))(span, item)
+            yield item
 
 
+    def get_time(self, span1, items):
+        """处理并获取时间"""
+        time_list = span1.xpath('./em/text()').extract()
+        items['start_time'] = time_list[0]
+        items['end_time'] = time_list[1]
 
+    def get_stations(self, span2, items):
+        """处理并获取车站信息"""
+        stations_list = span2.xpath('./em/text()').extract()
+        items['start_station'] = stations_list[0]
+        items['end_station'] = stations_list[1]
 
+    def code_spend(self, span3, items):
+        """处理并获取车号和花费时间"""
+        code_spend_list = span3.xpath('./em/text()').extract()
+        items['code'] = code_spend_list[0]
+        items['spend_time'] = code_spend_list[1]
+
+    def site_money_numbers(self, span4, items):
+        """处理并获取剩余票数以及价格"""
+        # 处理已发车的情况
+        try:
+            assert(len(span4.xpath('./em')) > 0)
+        except:
+            items['lowest_price'] = None
+            items['site_type'] = None
+            items['rest'] = None
+        else:
+            site_money_numbers_list = span4.xpath('./em')
+            items['lowest_price'] = site_money_numbers_list[0].xpath('./text()').extract_frist()
+            verbose = site_money_numbers_list[1]
+            items['site_type'] = verbose.xpath('./text()').extract_first()
+            items['rest'] = verbose.xpath('./i/text()').extract_first()
