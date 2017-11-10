48f9e7c63d184e46fa875fffa2a5177072491cc0
catsled <bigmaidong6@163.com>
2017-11-10 22:18:10 +0800

    fixed

--- splash_test_spider/splash_test_spider/spiders/test.py
+++ splash_test_spider/splash_test_spider/spiders/test.py
@@ -9,21 +9,17 @@ from scrapy_splash import SplashRequest
 class TestSpider(scrapy.Spider):
     name = 'test'
     allowed_domains = ['train.qunar.com']
-    start_urls = ['http://touch.train.qunar.com/trainList.html?startStation=%E5%8C%97%E4%BA%AC&endStation=%E4%B8%8A%E6%B5%B7&date=2017-11-10&searchType=stasta&bd_source=qunar&filterTrainType=&filterTrainType=&filterTrainType=']
+    start_urls = ['http://touch.train.qunar.com/trainList.html?startStation=%E5%8C%97%E4%BA%AC%E5%8D%97&endStation=%E4%B8%8A%E6%B5%B7%E8%99%B9%E6%A1%A5&date=2017-11-11']
 
     next_script = """
             function main(splash)
                 splash:set_user_agent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1Query String Parametersview sourceview URL encoded')
                 assert(splash:go(splash.args.url))
                 splash:wait(3)
-                local element = splash:select("#btn-calendar-next")
-                local html_now = splash:html()
-                element:mouse_click()
-                splash:wait(5)
-                return {
-                    url = splash:url(),
-                    html = html_now,
-                }
+                --local element = splash:select("#btn-calendar-next")
+                --element:mouse_click()
+                --splash:wait(5)
+                return { html = splash:html(), url = splash:url() }
             end
     """
 
@@ -42,12 +38,13 @@ class TestSpider(scrapy.Spider):
 
     def parse(self, response):
         ticket_info_list = response.xpath('//ul[@id="m-lists"]/li')
-        print('^'*20,response.url)
+        print(response.url)
         for ticket_info in ticket_info_list:
             item = SplashTestSpiderItem()
             ceil_info_list = ticket_info.xpath('./a/span')
             for index, span in enumerate(ceil_info_list, 1):
                 eval(self.tackle_method.get(index))(span, item)
+            print(item)
             yield item
 
 
@@ -80,7 +77,7 @@ class TestSpider(scrapy.Spider):
             items['rest'] = None
         else:
             site_money_numbers_list = span4.xpath('./em')
-            items['lowest_price'] = site_money_numbers_list[0].xpath('./text()').extract_frist()
+            items['lowest_price'] = site_money_numbers_list[0].xpath('./text()').extract_first()
             verbose = site_money_numbers_list[1]
             items['site_type'] = verbose.xpath('./text()').extract_first()
             items['rest'] = verbose.xpath('./i/text()').extract_first()
